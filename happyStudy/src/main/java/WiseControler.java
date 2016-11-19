d462ccd02bc4118cd58612c4b88f45531b3e39c7
xuehuagongzi <15022612930@163.com>
2016-11-19 20:28:50 +0800

    feture for conflict

--- happyStudy/src/main/java/WiseControler.java
+++ happyStudy/src/main/java/WiseControler.java
@@ -5,5 +5,6 @@ public class WiseControler {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("tomorrow i will study algorithm again");
+        System.out.println("tomorrow i will get up early,yes");
     }
 }
