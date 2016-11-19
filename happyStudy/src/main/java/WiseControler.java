e75e96d4499bc114691406eb48f8b3c8cf1d3971
xuehuagongzi <15022612930@163.com>
2016-11-19 20:30:48 +0800

    develop feture for conflict

--- happyStudy/src/main/java/WiseControler.java
+++ happyStudy/src/main/java/WiseControler.java
@@ -5,5 +5,13 @@ public class WiseControler {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("tomorrow i will study algorithm again");
+        System.out.println("tomorrow i am serious");
+        System.out.println("hello world");
+        System.out.println("tomorrow i will study algorithm again");
+        System.out.println("tomorrow i am serious");
+        System.out.println("hello world");
+        System.out.println("tomorrow i will study algorithm again");
+        System.out.println("tomorrow i am serious");
+
     }
 }
