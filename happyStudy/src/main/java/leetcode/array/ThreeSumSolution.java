1917101454ca7bd1e96df2dfe6d25c04ec2844ec
xuehuagongzi <15022612930@163.com>
2017-03-07 23:35:14 +0800

    leetcode update twoSumSolution and add ThreeSumSolution

---
+++ happyStudy/src/main/java/leetcode/array/ThreeSumSolution.java
@@ -0,0 +1,69 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by zhangwenqiang on 2017/3/7.
+ * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all
+ * unique triplets in the array which gives the sum of zero.
+ * Note: Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c) The solution set
+ * must not contain duplicate triplets.
+ * 题目翻译：
+ * 给定一个整型数组num， 找出这个数组中满足这个条件的所有数字： num[i]+num[j]+num[k] = 0. 并且所有
+ * 的答案是要和其他不同的， 也就是说两个相同的答案是不被接受的
+ * 题目的两点要求：
+ *    1. 每个答案组里面的三个数字是要从大到小排列起来的。
+ *    2. 每个答案不可以和其他的答案相同
+ */
+public class ThreeSumSolution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> listList=new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j=i+1;
+            int m=nums.length-1;
+            //要做好控制边界的量，否则会抛出数组越界异常
+            while (j<m){
+                if(nums[i]+nums[j]+nums[m] == 0) {
+                    List childList = new ArrayList<Integer>();
+                    childList.add(nums[i]);
+                    childList.add(nums[j]);
+                    childList.add(nums[m]);
+                    listList.add(childList);
+                    j++;
+                    //要做好控制边界的量，否则会抛出数组越界异常
+                    while (j < m && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    m--;
+                    //要做好控制边界的量，否则会抛出数组越界异常
+                    while (j < m && nums[m] == nums[m + 1]) {
+                        m--;
+                    }
+                }else if(nums[i]+nums[j]+nums[m] < 0){
+                    j++;
+                }else {
+                    m--;
+                }
+            }
+            //要做好控制边界的量，否则会抛出数组越界异常
+            while (i < nums.length-1&&nums[i+1]==nums[i]){
+                i++;
+            }
+        }
+        return listList;
+    }
+
+    public static void main(String[] args) {
+        int sum[]={-1,0,1,2,-1,-4};
+        List<List<Integer>> listList=threeSum(sum);
+        for (int i = 0; i < listList.size(); i++) {
+            for (int j = 0; j < listList.get(i).size(); j++) {
+                System.out.printf(listList.get(i).get(j).toString()+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
--- happyStudy/src/main/java/leetcode/array/TwoSumSolution.java
+++ happyStudy/src/main/java/leetcode/array/TwoSumSolution.java
@@ -1,56 +1,39 @@
 package leetcode.array;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
  * Created by zhangwenqiang on 2017/3/6.
  * Given an array of intergers, find two numbers such that they add up to a specific target number. The
- function twoSum should return indices of the two numbers such that they add up to the target,
- where index1 must be less than index2 Please note that your returned answers (both index1 and
- index2) are not zero-based.
- You may assume that each input would have exactly one solution.
- Input: numbers={2, 7, 11, 15}, target=9 Output: index1=1, index2=2
- 题目翻译： 这道题目的意思是给定一个数组和一个值， 让求出这个数组中两个值的和等于这个给定值的坐
- 标。 输出是有要求的， 1， 坐标较小的放在前面， 较大的放在后面。 2， 这俩坐标不能为零。
- */
+ * function twoSum should return indices of the two numbers such that they add up to the target,
+ * where index1 must be less than index2 Please note that your returned answers (both index1 and
+ * index2) are not zero-based.
+ * You may assume that each input would have exactly one solution.
+ * Input: numbers={2, 7, 11, 15}, target=9 Output: index1=1, index2=2
+ * 题目翻译： 这道题目的意思是给定一个数组和一个值， 让求出这个数组中两个值的和等于这个给定值的坐
+ * 标。 输出是有要求的， 1， 坐标较小的放在前面， 较大的放在后面。 2， 这俩坐标不能为零。
+ **/
 public class TwoSumSolution {
-    public static int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map=new LinkedHashMap<>();
-        int[] result=new int[2];
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
-        }
-        if(nums.length<1){
-            return result;
-        }
-        int i=0;
-        for(int key:map.keySet()){
-          if(map.containsKey(target - key)){
-                int index = map.get(target -key);
-                if (index == i) {
-                    continue;
-                } else {
-                    if (index > i) {
-                        result[0] = i+1;
-                        result[1] = index+1;
-                        break;
-                    } else {
-                        result[1] = i+1;
-                        result[0] = index+1;
-                        break;
-                    }
-                }
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            //避免了[3,3]  6,map中key重复的问题
+            if (map.containsKey(target - numbers[i])) {
+                //同时保证了result[1]存放的是较大的值
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
             }
-            i++;
+            map.put(numbers[i], i);
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int nums[]={10,3,5,0,16,9,15};
-        int res[]=twoSum(nums,19);
+        int nums[]={3,3};
+        int res[]=twoSum(nums,6);
         System.out.println(res[0]);
         System.out.println(res[1]);
     }
